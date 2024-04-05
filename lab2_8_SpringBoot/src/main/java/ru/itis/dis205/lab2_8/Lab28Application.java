a81ae992586f118d2df825e371a46c9a65d4d7e5
ksenikeev <ksenikeev@mail.ru>
2024-04-05 17:08:13 +0300

    SpringBoot

---
+++ lab2_8_SpringBoot/src/main/java/ru/itis/dis205/lab2_8/Lab28Application.java
@@ -0,0 +1,11 @@
+package ru.itis.dis205.lab2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab28Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Lab28Application.class, args);
+	}
+}
